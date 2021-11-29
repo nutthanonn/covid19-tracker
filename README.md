ef2ba76612af09eb11cad86b67238e4a73e38428
nutthanon <nutthanon.tho@gmail.com>
2021-11-29 18:48:09 +0700

    [+] simple-vue

--- README.md
+++ README.md
@@ -1,25 +1,7 @@
 # simple-vuejs
 
-## Project setup
-```
-npm install
-```
-
 ### Compiles and hot-reloads for development
-```
-npm run serve
-```
 
-### Compiles and minifies for production
 ```
-npm run build
+npm run start
 ```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
-# basic-vuejs
--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <v-btn @click="increase">Click</v-btn>
+    <p>{{ count }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+
+  data: () => ({
+    count: 1,
+  }),
+  methods: {
+    increase() {
+      return (this.count += 1);
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
