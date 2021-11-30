ede24515e5d62504acf3e9fb53a8919f823d9c73
nutthanon <nutthanon.tho@gmail.com>
2021-11-30 20:00:18 +0700

    [+] styled-components

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "antd";
+import styled from "styled-components";
+
+const btn = styled.div`
+  background-color: red;
+`;
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default App;
